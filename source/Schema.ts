ce15fa20306ee4acbebfae94b0c5619d0cf95d17
Jeremy <jeremy.bankes@gmail.com>
2023-01-22 12:54:00 -0400

    Added source typing support

--- source/Schema.ts
+++ source/Schema.ts
@@ -1,282 +1,213 @@
-/**
- * A mapping of names to their corresponding types.
- * Used for static analysis.
- */
-export interface TypeMap {
-    "string": string,
-    "number": number,
+const PRIMITIVES = [
+    "undefined",
+    "boolean",
+    "number",
+    "bigint",
+    "string",
+    "symbol",
+    "object",
+    "Date"
+];
+
+export interface Primitives {
+    "undefined": undefined,
     "boolean": boolean,
-    "Date": Date,
-    "any": any
+    "number": number,
+    "bigint": bigint,
+    "string": string,
+    "symbol": symbol,
+    "object": object,
+    "Date": Date
 }
 
-/**
- * Typing for a function that converts a given value to a different type.
- */
-export type TypeConverter<FromType, ToType> = (value: FromType) => ToType;
+namespace Schema {
+    export type Primitive = keyof Primitives;
+    export type Meta = {
+        type: All,
+        required: boolean,
+        default?: any
+    };
+    export type Array = [All];
+    export type Hierarchy = { [Key: string]: All };
+    export type All = Primitive | Meta | Array | Hierarchy;
+}
 
-/**
- * A mapping of type converters.
- * A key in the first level hierarchy represents the type of source data.
- * A key in the second level hierarchy represents the type of converted data.
- */
-type TypeConverterMap = (
-    { [Key: string]: { [Key: string]: TypeConverter<any, any> } }
-);
-
-/**
- * Typing for possible validation error types.
- * @see ValidationError.type
- */
-export type ValidationErrorType = "missing" | "incorrectType";
-
-/**
- * Meta describing a single value within an object. Used extensively within Schemas.
- * @see Schema
- */
-export type SchemaItem = {
-    type: Schema,
-    required: boolean,
-    default?: any
-};
-
-/**
- * A hierarchy describing the structure of an object.
- */
-export type SchemaHierarchy = { [Key: string]: Schema };
-
-/**
- * An item that describes the structure, type or layout of data.
- */
-export type Schema = keyof TypeMap | SchemaItem | [Schema] | SchemaHierarchy;
-
-/**
- * Represents the typing for an object that has been validated to match the "Layout" Schema.
- * @see Schema
- */
-export type Model<Layout extends Schema> = (
-    Layout extends keyof TypeMap ? TypeMap[Layout] :
-    Layout extends SchemaItem ? Model<Layout["type"]> :
-    Layout extends [Schema] ? Model<Layout["0"]>[] :
-    Layout extends SchemaHierarchy ? (
-        { [Key in keyof Layout as Exists<Layout[Key]> extends true ? Key : never]: Model<Layout[Key]> } &
-        { [Key in keyof Layout as Exists<Layout[Key]> extends true ? never : Key]?: Model<Layout[Key]> }
+// If you're wondering while I'm wrapping my types in Tuples, me too.
+// https://stackoverflow.com/questions/75188805/how-should-one-avoid-excessively-deep-type-instantiation-when-attemping-to-creat
+export type Model<Schema extends Schema.All> = (
+    [Schema] extends [Schema.Primitive] ? Primitives[Schema] :
+    [Schema] extends [Schema.Meta] ? Model<Schema["type"]> :
+    [Schema] extends [Schema.Array] ? Model<Schema[0]>[] :
+    [Schema] extends [Schema.Hierarchy] ? (
+        { [Key in keyof Schema as Existant<Schema[Key]> extends true ? Key : never]: Model<Schema[Key]> } &
+        { [Key in keyof Schema as Existant<Schema[Key]> extends true ? never : Key]?: Model<Schema[Key]> }
     ) :
     never
 );
 
-/**
- * Determines if a Schema is required.
- * By default values are not required unless otherwise specified in the meta of a SchemaItem.
- * @see SchemaItem
- * @see Schema
- */
-type Exists<Layout extends Schema> = (
-    Layout extends SchemaItem ? (Layout["required"] extends true ? true : (Layout extends { default: any } ? true : false)) :
-    Layout extends [Schema] ? Exists<Layout[0]> :
-    Layout extends SchemaHierarchy ? { [Key in keyof Layout]: Exists<Layout[Key]> } extends { [Key in keyof Layout]: false } ? false : true :
+export type Source<Schema extends Schema.All> = (
+    [Schema] extends [Schema.Primitive] ? Primitives[Schema] :
+    [Schema] extends [Schema.Meta] ? Source<Schema["type"]> :
+    [Schema] extends [Schema.Array] ? Source<Schema[0]>[] :
+    [Schema] extends [Schema.Hierarchy] ? (
+        { [Key in keyof Schema as Optionality<Schema[Key]> extends true ? Key : never]?: Source<Schema[Key]> } &
+        { [Key in keyof Schema as Optionality<Schema[Key]> extends true ? never : Key]: Source<Schema[Key]> }
+    ) :
+    never
+);
+
+type Existant<Schema extends Schema.All> = (
+    Schema extends Schema.Meta ? (Schema["required"] extends true ? true : (Schema extends { default: any } ? true : false)) :
+    Schema extends Schema.Array ? Existant<Schema[0]> :
+    Schema extends Schema.Hierarchy ? { [Key in keyof Schema]: Existant<Schema[Key]> } extends { [Key in keyof Schema]: false } ? false : true :
     false
 );
 
-const TYPE_CONVERTER_MAP: TypeConverterMap = {
-    string: {
-        number: (value) => parseFloat(value),
-        boolean: (value) => Boolean(value),
-        Date: (value) => new Date(value),
-        any: (value) => JSON.parse(value)
-    },
-    number: {
-        string: (value) => value.toString(),
-        boolean: (value) => value !== 0,
-        Date: (value) => new Date(value)
-    },
-    boolean: {
-        string: (value) => value ? 'true' : 'false',
-        number: (value) => value ? 1 : 0
-    },
-    Date: {
-        string: (value) => value.toString(),
-        number: (value) => value.getTime()
-    }
-};
+type Optionality<Schema extends Schema.All> = (
+    Schema extends Schema.Meta ? (Schema["required"] extends true ? (Schema extends { default: any } ? true : false) : true) :
+    Schema extends Schema.Array ? Optionality<Schema[0]> :
+    Schema extends Schema.Hierarchy ? { [Key in keyof Schema]: Optionality<Schema[Key]> } extends { [Key in keyof Schema]: false } ? false : true :
+    false
+);
 
-/**
- * Used to define a metadata object that represents the structure and typing of an object. 
- * @see Schema
- * 
- * @param schema The metadata describing the structure and typing of an object.
- * @returns A schema that can be used to validate an object with the {@link validate} function.
- */
-export function build<Layout extends Schema>(schema: Layout): Layout {
+function isSchemaPrimitive(value: any): value is Schema.Primitive {
+    if (typeof value !== "string") {
+        return false;
+    }
+    return PRIMITIVES.includes(value);
+}
+
+function isSchemaMeta(value: any): value is Schema.Meta {
+    if (typeof value !== "object" || value === null) {
+        return false;
+    }
+    return isSchema(value.type) && typeof value.required === "boolean";
+}
+
+function isSchemaArray(value: any): value is Schema.Array {
+    if (typeof value !== "object" || !Array.isArray(value) || value.length !== 1) {
+        return false;
+    }
+    return isSchema(value[0]);
+}
+
+function isSchemaHierarchy(value: any): value is Schema.Hierarchy {
+    if (typeof value !== "object" || value === null) {
+        return false;
+    }
+    for (const key in value) {
+        if (!isSchema(value[key])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isSchema(value: any): boolean {
+    return isSchemaPrimitive(value) || isSchemaMeta(value) || isSchemaArray(value) || isSchemaHierarchy(value);
+}
+
+function isOptional<Schema extends Schema.All>(schema: Schema): boolean {
+    if (isSchemaPrimitive(schema)) {
+        return false;
+    } if (isSchemaMeta(schema)) {
+        return !schema.required || "default" in schema;
+    } else if (isSchemaArray(schema)) {
+        return isOptional(schema[0]);
+    } else if (isSchemaHierarchy(schema)) {
+        for (const key in schema) {
+            if (!isOptional(schema[key])) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        throw new Error("Invalid schema.");
+    }
+}
+
+export function build<Layout extends Schema.All>(schema: Layout): Layout {
     return schema;
 }
 
-function validateByKeyOfTypeMap<Layout extends Schema>(value: any, schema: keyof TypeMap, path: string[]): Model<Layout> {
-    const dataType = typeof value;
-    if (dataType !== schema) {
-        const converters = TYPE_CONVERTER_MAP[dataType];
-        if (converters !== undefined) {
-            const converter = converters[schema];
-            if (converter !== undefined) {
-                return converter(value);
+export function validate<Schema extends Schema.All>(source: Source<Schema>, schema: Schema, path: string[] = []): Model<Schema> {
+    if (isSchemaPrimitive(schema)) {
+        if (typeof source !== schema) {
+            if (!(source instanceof Object) || source.constructor.name !== schema) {
+                throw new ValidationError("incorrectType", source, schema, path);
             }
         }
-        if (value === undefined) {
-            throw new ValidationError("missing", value, schema, path);
-        } else {
-            throw new ValidationError("incorrectType", value, schema, path);
-        }
-    }
-    return value;
-}
-
-function validateBySchemaItem<Layout extends Schema>(value: any, schema: SchemaItem, path: string[]): Model<Layout> {
-    try {
-        // @ts-ignore
-        return validate(value, schema.type, path);
-    } catch (error) {
-        if (error instanceof ValidationError) {
-            if ("default" in schema) {
-                if (typeof schema.default === "function") {
-                    return validate(schema.default(), schema.type, path);
-                } else {
-                    return validate(schema.default, schema.type, path);
-                }
-            } else if (schema.required) {
-                throw error;
-            }
-        }
-        throw error;
-    }
-}
-
-function validateBySchemaArray<Layout extends Schema>(value: any, schema: [Schema], path: string[]): Model<Layout> {
-    if (!Array.isArray(value)) {
-        throw new ValidationError('incorrectType', value, schema, path);
-    }
-    const data: any = [];
-    for (let i = 0; i < value.length; i++) {
-        data[i] = validate(value[i], schema[0], [...path, i.toString()]);
-    }
-    return data;
-}
-
-function validateBySchemaHierarchy<Layout extends Schema>(value: any, schema: SchemaHierarchy, path: string[]): Model<Layout> {
-    if (typeof value !== 'object' || value === null) {
-        throw new ValidationError('incorrectType', value, schema, path);
-    }
-    const data: any = {};
-    for (const key in schema) {
+        return source as unknown as Model<Schema>;
+    } else if (isSchemaMeta(schema)) {
         try {
-            data[key] = validate(value[key], schema[key], [...path, key]);
+            return validate<Schema>(source, schema.type as Schema, path) as unknown as Model<Schema>;
         } catch (error) {
             if (error instanceof ValidationError) {
-                if (isExistent(schema[key])) {
+                if ("default" in schema) {
+                    if (typeof schema.default === "function") {
+                        return validate<Schema>(schema.default(), schema.type as Schema, path) as Model<Schema>;
+                    } else {
+                        return validate<Schema>(schema.default, schema.type as Schema, path) as Model<Schema>;
+                    }
+                } else if (schema.required) {
                     throw error;
                 }
-            } else {
-                throw error;
             }
+            throw error;
         }
-    }
-    return data as Model<Layout>;
-}
-
-/**
- * Validates that a given 'value' matches a given 'schema' and evaluates any type conversions or defaults.
- * @see Schema
- * 
- * @param value A value to test the structure and type of against 'schema'.
- * @param schema The schema used to validate 'value'.
- * @param path Used within the implementation and can be ignored for external use.
- * @returns A validated and evaluated copy of 'value'
- */
-export function validate<Layout extends Schema>(value: any, schema: Layout, path: string[] = []): Model<Layout> {
-    if (isKeyOfTypeMap(schema)) {
-        return validateByKeyOfTypeMap<Layout>(value, schema, path);
-    } else if (isSchemaItem(schema)) {
-        return validateBySchemaItem<Layout>(value, schema, path);
     } else if (isSchemaArray(schema)) {
-        return validateBySchemaArray<Layout>(value, schema, path);
+        if (!Array.isArray(source)) {
+            throw new ValidationError("incorrectType", source, schema, path);
+        }
+        const validated: any = [];
+        for (let i = 0; i < source.length; i++) {
+            validated[i] = validate<Schema>(source[i], schema[0] as Schema, [...path, i.toString()]);
+        }
+        return validated as Model<Schema>;
     } else if (isSchemaHierarchy(schema)) {
-        return validateBySchemaHierarchy<Layout>(value, schema, path);
-    } else {
-        throw new Error(`Unsure how to interpret schema. ${JSON.stringify(schema)}`);
-    }
-}
-
-function isKeyOfTypeMap(schema: Schema): schema is keyof TypeMap {
-    return typeof schema === "string";
-}
-
-function isSchemaItem(schema: Schema): schema is SchemaItem {
-    return typeof schema === "object" && !Array.isArray(schema) && typeof schema.required === "boolean" && isSchema(schema.type);
-}
-
-function isSchemaArray(schema: Schema): schema is [Schema] {
-    return Array.isArray(schema) && isSchema(schema[0]);
-}
-
-function isSchemaHierarchy(schema: Schema): schema is SchemaHierarchy {
-    return !isSchemaItem(schema);
-}
-
-function isSchema(value: any): value is Schema {
-    return isKeyOfTypeMap(value) || isSchemaItem(value) || isSchemaArray(value) || isSchemaHierarchy(value);
-}
-
-function isExistent(schema: Schema): boolean {
-    if (isSchemaItem(schema)) {
-        return schema.required || schema.default;
-    } else if (isSchemaArray(schema)) {
-        return isExistent(schema[0]);
-    } else if (isSchemaHierarchy(schema)) {
+        if (typeof source !== "object" || source === null) {
+            throw new ValidationError("incorrectType", source, schema, path);
+        }
+        const validated: any = {};
         for (const key in schema) {
-            if (isExistent(schema[key])) {
-                return true;
+            try {
+                if (!(key in source) && isOptional(schema[key]) && isSchemaHierarchy(schema[key])) {
+                    (source as any)[key] = {} as any;
+                }
+                validated[key] = validate<Schema>((source as any)[key], schema[key] as Schema, [...path, key]);
+            } catch (error) {
+                if (!(error instanceof ValidationError) || !isOptional(schema[key])) {
+                    throw error;
+                }
             }
         }
+        return validated as Model<Schema>;
+    } else {
+        throw new Error("Invalid schema.");
     }
-    return false;
 }
 
-/**
- * Registers a type converter used for runtime type conversions during validations with {@link validate}.
- *  
- * @param fromTypeName The name of the type to convert from.
- * @param toTypeName The name of the type to convert to.
- * @param conversionCallback A callback that can convert the type represented by 'fromTypeName' to the type represented by 'toTypeName'.
- */
-export function registerTypeConversion<FromType, ToType>(fromTypeName: string, toTypeName: string, conversionCallback: TypeConverter<FromType, ToType>) {
-    if (!(fromTypeName in TYPE_CONVERTER_MAP)) {
-        TYPE_CONVERTER_MAP[fromTypeName] = {};
-    }
-    TYPE_CONVERTER_MAP[fromTypeName][toTypeName] = conversionCallback;
-}
+export type ValidationErrorType = "missing" | "incorrectType";
 
-/**
- * Represents and error that occures while validating an object.
- * @see validate
- */
 export class ValidationError extends Error {
 
     private _type: ValidationErrorType;
     private _data: object;
-    private _schema: Schema;
+    private _schema: Schema.All;
     private _path: string[];
 
     /**
      * @param message The error message.
      * @param type The type of validation error.
-     * @param data The data that failed validation.
+     * @param source The data that failed validation.
      * @param path The path to the value that failed validation.
      */
-    public constructor(type: ValidationErrorType, data: any, schema: Schema, path: string[]) {
-        super(ValidationError._buildMessage(type, data, schema, path));
+    public constructor(type: ValidationErrorType, source: any, schema: Schema.All, path: string[]) {
+        super(ValidationError._buildMessage(type, source, schema, path));
         this._type = type;
         this._path = path;
         this._schema = schema;
-        this._data = data;
+        this._data = source;
     }
 
     public get type() { return this._type; }
@@ -284,7 +215,7 @@ export class ValidationError extends Error {
     public get schema() { return this._schema; }
     public get data() { return this._data; }
 
-    private static _buildMessage(type: ValidationErrorType, data: any, schema: Schema, path: string[]): string {
+    private static _buildMessage(type: ValidationErrorType, data: any, schema: Schema.All, path: string[]): string {
         if (path.length === 0) {
             return `Attempted to validate ${JSON.stringify(data)}`;
         }
@@ -297,4 +228,5 @@ export class ValidationError extends Error {
                 return `Validation failed. Reason: ${type}`;
         }
     }
+
 }
